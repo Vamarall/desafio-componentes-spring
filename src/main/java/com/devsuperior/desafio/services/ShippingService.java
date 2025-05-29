29ec1c874175346ec6795f62a07591b6a667dcee
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-05-29 19:52:17 -0300

    Created ShippingService

---
+++ src/main/java/com/devsuperior/desafio/services/ShippingService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.desafio.services;
+
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.desafio.entities.Order;
+
+@Service
+public class ShippingService {
+
+    public double shipment(Order order) {
+        double basic = order.getBasic();
+    
+        if (basic < 100.0) {
+            return 20.0;
+        } else if (basic <= 200.0) {
+            return 12.0;
+        } else {
+            return 0.0;
+        }
+    }
+    
+
+}
